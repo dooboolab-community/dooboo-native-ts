e26d3d9af47089708e0e05d67f9a4b8bbc851e25
hyochan <dooboolab@gmail.com>
2018-03-01 16:38:34 +0900

    Updated package name and dir for ios.

--- ios/DoobooNativeTests/DoobooNativeTests.m
+++ ios/DoobooNativeTests/DoobooNativeTests.m
@@ -16,11 +16,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface ReactNativeTypeScriptStarterTests : XCTestCase
+@interface DoobooNativeTests : XCTestCase
 
 @end
 
-@implementation ReactNativeTypeScriptStarterTests
+@implementation DoobooNativeTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
