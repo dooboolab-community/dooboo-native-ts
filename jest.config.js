d5a0ae8eb994383bfadd40d1122dcbb318307f10
hyochan <dooboolab@gmail.com>
2019-07-08 12:00:32 +0900

    Fix lint

--- jest.config.js
+++ jest.config.js
@@ -2,42 +2,42 @@ const { defaults: tsJestConfig } = require('ts-jest/presets');
 
 module.exports = {
   ...tsJestConfig,
-  "preset": "react-native",
-  "transformIgnorePatterns": [
-    "node_modules/(?!(.*-)?react-(.*-)?native(-.*)?)"
+  'preset': 'react-native',
+  'transformIgnorePatterns': [
+    'node_modules/(?!(.*-)?react-(.*-)?native(-.*)?)',
   ],
-  "setupFiles": [
-    "./test/jestSetup.ts"
+  'setupFiles': [
+    './test/jestSetup.ts',
   ],
-  "globals": {
-    "window": {},
-    "ts-jest": {
-      "babelConfig": false,
-      "tsConfig": "tsconfig.jest.json"
-    }
+  'globals': {
+    'window': {},
+    'ts-jest': {
+      'babelConfig': false,
+      'tsConfig': 'tsconfig.jest.json',
+    },
   },
-  "transform": {
-    "^.+\\.(js|jsx)$": "<rootDir>/node_modules/babel-jest",
-    "\\.(ts|tsx)$": "ts-jest"
+  'transform': {
+    '^.+\\.(js|jsx)$': 'babel-jest',
+    '\\.(ts|tsx)$': 'ts-jest',
   },
-  // "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
-  "testPathIgnorePatterns": [
-    "\\.snap$",
-    "<rootDir>/node_modules/"
+  // 'testRegex': '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+  'testPathIgnorePatterns': [
+    '\\.snap$',
+    '<rootDir>/node_modules/',
   ],
-  "cacheDirectory": ".jest/cache",
-  "moduleFileExtensions": [
-    "ts",
-    "tsx",
-    "js",
-    "jsx",
-    "json",
-    "ios.ts",
-    "ios.tsx",
-    "android.ts",
-    "android.tsx"
+  'cacheDirectory': '.jest/cache',
+  'moduleFileExtensions': [
+    'ts',
+    'tsx',
+    'js',
+    'jsx',
+    'json',
+    'ios.ts',
+    'ios.tsx',
+    'android.ts',
+    'android.tsx',
   ],
-  // "moduleNameMapper": {
-  //   "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/test/assetsTransformer.js"
+  // 'moduleNameMapper': {
+  //   '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/test/assetsTransformer.js'
   // },
 };
