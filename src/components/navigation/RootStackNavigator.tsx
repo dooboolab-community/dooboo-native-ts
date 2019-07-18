68bf266dbb2009415767572d3df8ba0430cf1d91
hyochan <dooboolab@gmail.com>
2019-07-18 15:44:17 +0900

    Upgrade templates to react-native 0.60.3

--- src/components/navigation/RootStackNavigator.tsx
+++ src/components/navigation/RootStackNavigator.tsx
@@ -2,23 +2,39 @@ import React from 'react';
 import { Text } from 'react-native';
 import { createStackNavigator, NavigationRouteConfig, StackNavigatorConfig } from 'react-navigation';
 
-import IntroScreen from '../screen/Intro';
-import TempScreen from '../screen/Temp';
+import Intro from '../screen/Intro';
+import Temp from '../screen/Temp';
 
 const routeConfig: NavigationRouteConfig = {
   Intro: {
-    screen: IntroScreen,
-    navigationOptions: {
-      title: 'Intro',
+    screen: Intro,
+    navigationOptions: ({ navigation, screenProps } : { navigation: any, screenProps: any }) => {
+      const { theme } = screenProps;
+      return ({
+        title: navigation.state.routeName,
+        headerStyle: {
+          backgroundColor: theme.background,
+        },
+        headerTitleStyle: { color: theme.fontColor },
+        headerTintColor: theme.tintColor,
+      });
     },
     path: 'intro',
   },
   Temp: {
-    screen: TempScreen,
-    navigationOptions: {
-      headerTitle: <Text style={{
-        fontSize: 18,
-      }}>Temp</Text>,
+    screen: Temp,
+    navigationOptions: ({ navigation, screenProps } : { navigation: any, screenProps: any }) => {
+      const { theme } = screenProps;
+      return ({
+        headerTitle: <Text style={{
+          fontSize: 18,
+        }}>{navigation.state.routeName}</Text>,
+        headerStyle: {
+          backgroundColor: theme.background,
+        },
+        headerTitleStyle: { color: theme.fontColor },
+        headerTintColor: theme.tintColor,
+      });
     },
     path: 'temp',
   },
@@ -26,40 +42,29 @@ const routeConfig: NavigationRouteConfig = {
 
 const navigatorConfig: StackNavigatorConfig = {
   initialRouteName: 'Intro',
+  mode: 'card',
+  headerMode: 'screen',
   // headerMode: 'none',
-  navigationOptions: ({ navigation, screenProps } : { navigation: any, screenProps: any}) => {
-    const { theme } = screenProps;
-    return ({
-      headerStyle: {
-        backgroundColor: theme.background,
-        borderBottomColor: 'transparent',
-        borderBottomWidth: 0,
-        elevation: 0,
-      },
-      headerTitleStyle: { color: theme.fontColor },
-      headerTintColor: theme.tintColor,
-    });
-  },
 };
 
 const RootStackNavigator = createStackNavigator(routeConfig, navigatorConfig);
 
-interface IProps {
-  navigation?: any;
-  theme?: object;
-}
+// interface IProps {
+//   navigation?: any;
+//   theme?: object;
+// }
 
-class RootNavigator extends React.Component<IProps> {
-  private static router = RootStackNavigator.router;
+// class RootNavigator extends React.Component<IProps> {
+//   private static router = RootStackNavigator.router;
 
-  public render() {
-    return (
-      <RootStackNavigator
-        navigation={this.props.navigation}
-        screenProps={{ theme: this.props.theme }}
-      />
-    );
-  }
-}
+//   public render() {
+//     return (
+//       <RootStackNavigator
+//         navigation={this.props.navigation}
+//         screenProps={{ theme: this.props.theme }}
+//       />
+//     );
+//   }
+// }
 
-export default RootNavigator;
+export default RootStackNavigator;
