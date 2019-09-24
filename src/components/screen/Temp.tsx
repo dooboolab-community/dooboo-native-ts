89b6ba409bca7ec8f46b5c3cde6267c78ba03574
Gordon Freeman <godon019@gmail.com>
2019-09-24 19:22:19 +0900

    Fix some type errors

--- src/components/screen/Temp.tsx
+++ src/components/screen/Temp.tsx
@@ -1,17 +1,18 @@
 import Button from '../shared/Button';
+import { NavigationScreenProp } from 'react-navigation';
 import React from 'react';
 import styled from 'styled-components/native';
 
 const Container = styled.View`
   flex: 1;
-  background-color: ${(props): stirng => props.theme.background};
+  background-color: ${(props): string => props.theme.background};
   flex-direction: row;
   align-items: center;
   justify-content: center;
 `;
 
 interface Props {
-  navigation: NavigationScreenProp<NavigationStateRoute>;
+  navigation: NavigationScreenProp<any, any>;
 }
 
 function Page(props: Props): React.ReactElement {
@@ -19,7 +20,7 @@ function Page(props: Props): React.ReactElement {
     <Container>
       <Button
         testID='btn'
-        onClick={(): string => props.navigation.goBack()}
+        onClick={(): boolean => props.navigation.goBack()}
         text='Go Back'
         style={{
           backgroundColor: '#333333',
