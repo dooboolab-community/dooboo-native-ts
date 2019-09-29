0d753d3f6703cfab42eb7651796d49a9b5eacd04
Gordon Freeman <godon019@gmail.com>
2019-09-29 21:46:04 +0900

    Refactor ThemeProvider

--- src/providers/ThemeProvider.tsx
+++ src/providers/ThemeProvider.tsx
@@ -1,38 +1,45 @@
 import React, { useState } from 'react';
-import { Theme, ThemeType, createTheme } from 'theme';
+import { Theme, ThemeType, createTheme } from '../theme';
 
-import { ThemeProvider as OriginalThemeProvider } from 'styled-components';
-import createCtx from 'utils/createCtx';
+import { ThemeProvider as OriginalThemeProvider } from 'styled-components/native';
+import createCtx from '../utils/createCtx';
 
 interface Context {
   theme: Theme;
   themeType: ThemeType;
-  changeTheme: React.Dispatch<React.SetStateAction<ThemeType>>;
+  changeThemeType: () => void;
 }
 const [useCtx, Provider] = createCtx<Context>();
 
-const initialThemeType: ThemeType = ThemeType.LIGHT;
+const defaultThemeType: ThemeType = ThemeType.LIGHT;
 
 interface Props {
   children?: React.ReactElement;
+  // using initial ThemeType is essential while testing apps with consistent ThemeType
+  initialThemeType?: ThemeType;
 }
 
-function ThemeProvider(props: Props): React.ReactElement {
-  const [themeType, changeTheme] = useState(initialThemeType);
+function ThemeProvider({
+  children,
+  initialThemeType = defaultThemeType,
+}: Props): React.ReactElement {
+  const [themeType, setThemeType] = useState(initialThemeType);
+  const changeThemeType = (): void => {
+    setThemeType(ThemeType.LIGHT ? ThemeType.DARK : ThemeType.LIGHT);
+  };
   const theme = createTheme(themeType);
+
   return (
     <Provider
       value={{
-        changeTheme,
+        changeThemeType,
         themeType,
         theme,
       }}
     >
-      <OriginalThemeProvider theme={theme}>
-        {props.children}
-      </OriginalThemeProvider>
+      <OriginalThemeProvider theme={theme}>{children}</OriginalThemeProvider>
     </Provider>
   );
 }
 
-export { useCtx as useThemeProvicer, ThemeProvider };
+export { useCtx as useThemeProvider, ThemeProvider };
