3586d47a2ccf79afac7ad9ba5232689c4e93dd60
hyochan <dooboolab@gmail.com>
2021-01-01 23:22:10 +0900

    Configure react-native-web in project

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import { AppRegistry, Platform } from 'react-native';
+
+import App from './App';
+
+AppRegistry.registerComponent('dooboo', () => App);
+
+if (Platform.OS === 'web') {
+  AppRegistry.runApplication('dooboo', {
+    rootTag: document.getElementById('root'),
+  });
+}
+
+export default App;
