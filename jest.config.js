f2dc8faf2bbcbeac7bc61fb95bf97310decdd957
hyochan <dooboolab@gmail.com>
2021-01-01 23:22:10 +0900

    Configure react-native-web in project

--- jest.config.js
+++ jest.config.js
@@ -7,6 +7,8 @@ module.exports = {
   preset: 'react-native',
   automock: false,
   transform: {
+    // eslint-disable-next-line
+    '^.+\\.(bmp|gif|jpg|jpeg|mp4|png|psd|svg|webp|ttf|otf|m4v|mov|mp4|mpeg|mpg|webm|aac|aiff|caf|m4a|mp3|wav|html|pdf|obj)$': '<rootDir>/src/mocks/Files/index.js',
     '^.+\\.(js)$': '<rootDir>/node_modules/babel-jest',
     '\\.ts$': 'ts-jest',
   },
