2098769983adaa5ff0821e63be2c7ea0caa7250e
Hyo <dooboolab@gmail.com>
2021-07-06 18:09:49 +0900

    Update react-native async storage module

--- test/jestSetup.ts
+++ test/jestSetup.ts
@@ -5,9 +5,9 @@ import {initFbt} from '../src/utils/fbt';
  * Something went wrong initializing the native ReactLocalization module
  * https://gist.github.com/MoOx/08b465c3eac9e36e683929532472d1e0
  */
-import mockAsyncStorage from '@react-native-community/async-storage/jest/async-storage-mock';
+import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock';
 
-jest.mock('@react-native-community/async-storage', () => mockAsyncStorage);
+jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
 
 jest.mock('react-native-reanimated', () =>
   require('react-native-reanimated/mock'),
