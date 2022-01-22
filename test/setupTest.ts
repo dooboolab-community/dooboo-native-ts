7f0ab96974ebd62e1830790ba3500060a72c9a10
Hyo <dooboolab@gmail.com>
2022-01-22 16:33:14 +0900

    React Native 0.66.3 Boilerplate (#178)
    * Fix broken tests
    
    * Update yarn.lock

--- test/setupTest.ts
+++ test/setupTest.ts
@@ -3,6 +3,8 @@ import {cleanup} from '@testing-library/react-native';
 // Cleanup after each case.
 afterEach(cleanup);
 
+Date.now = jest.fn(() => new Date('2020-01-01T04:30:54.591Z').valueOf());
+
 process.on('unhandledRejection', (err) => {
   fail(err);
 });
